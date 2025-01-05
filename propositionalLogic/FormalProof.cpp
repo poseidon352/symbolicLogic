ba10e83b9e5ed436ec7f49732d1e8b40577a1189
samtaplin <staplin@student.ubc.ca>
2025-01-05 13:50:41 -0800

    Implement FormalProof class methods for premise/conclusion setup and conditional proof handling.

--- propositionalLogic/FormalProof.cpp
+++ propositionalLogic/FormalProof.cpp
@@ -1,8 +1,60 @@
 #include "FormalProof.h"
+#include <cassert>
+#include <stdexcept>
+#include <iostream>
 
-FormalProof::FormalProof(Node* root): root(root) {}
+FormalProof::FormalProof(Node* root): root(root) {
+    setPremiseConclusion();
+    prove(root);
+}
 
-void FormalProof::prove(Node* currNode, std::string proof) {
-    if (root->left == root->right) return;
+void FormalProof::setPremiseConclusion() {
+    assert(root && "root has not has not been initialized");
 
+    switch (root->type) {
+        case VAR: premise, conclusion = root;
+                  break;
+        case CON: premise = root->left;
+                  conclusion = root->right;
+                  break;
+        case BICON: // TODO
+                  break;
+        default: throw std::runtime_error("Incorrect root Type");
+    }
+}
+
+void FormalProof::prove(Node* currNode) {
+    if (root->type == CON) {
+        conditionalProof(currNode);
+    }
+
+}
+
+void FormalProof::conditionalProof(Node* currNode) {
+    Node* lhs = currNode->left;
+    Node* rhs = currNode->right;
+
+    assumptions.push_back(lhs);
+    steps.push_back(new Step(lhs, ASSUMPTION, 0));
+    toProve.push(rhs);
+    if (lhs->type == VAR && rhs->type == OR) {
+        lhsVarRhsOr(currNode);
+    }
+}
+
+void FormalProof::lhsVarRhsOr(Node* currNode) {
+    Node* varNode = currNode->left;
+    Node* orNode = currNode->right;
+
+    Step* newStep;
+    if (varNode == orNode->left) {
+        newStep = new Step(orNode, ORintro, 1);
+    }
+
+    steps.push_back(newStep);
+    if (newStep->expression == toProve.front()) {
+        toProve.pop();
+    }
+
+    // TODO: Deal with condition that neither are true
 }
--- propositionalLogic/FormalProof.h
+++ propositionalLogic/FormalProof.h
@@ -2,29 +2,55 @@
 #define _FORMALPROOF_H_
 
 #include "Node.h"
+#include <queue>
 
 enum Rule {
-    NOTI,
-    ANDI,
-    ORI,
-    CONI,
-    BICONI,
-    NOTE,
-    ANDE,
-    ORE,
-    CONE,
-    BICONE,
-    FALSE
+    NOTintro,
+    ANDintro,
+    ORintro,
+    CONintro,
+    BICONintro,
+    CONTRADICTIONintro,
+    NOTelim,
+    ANDelim,
+    ORelim,
+    CONelim,
+    BICONelim,
+    CONTRADICTIONelim,
+    ASSUMPTION,
+    CONCLUSION
 };
 
 class FormalProof {
 public:
     FormalProof(Node* root);
 
-    void prove(Node* currNode, std::string proof);
+    void prove(Node* currNode);
 
 private:
-   Node* root;
+    class Step {
+    public:
+        Node* expression;
+        Rule rule;
+        int depth;
 
+        Step(Node* expression, Rule rule, int depth);
+    
+    };
+
+   Node* root;
+   Node* premise;
+   Node* conclusion;
+   std::vector<Step*> steps;
+   std::vector<Node*> assumptions;
+   std::queue<Node*> toProve;
+
+   void setPremiseConclusion();
+
+   void conditionalProof(Node* currNode);
+
+   void lhsVarRhsOr(Node* currNode);
+
+   bool satisfiesRule(Rule rule, std::vector<Step*> procedure);
 };
 #endif
